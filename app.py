9dc1633483a19917e3936edc93f17d9ba00b9066
CaoKimDo <135789731+CaoKimDo@users.noreply.github.com>
2024-03-22 09:53:06 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import counterfit_shims_serial
+import pynmea2
+import time
+
+from counterfit_connection import CounterFitConnection
+CounterFitConnection.init('127.0.0.1', 5000)
+
+serial = counterfit_shims_serial.Serial('/dev/ttyAMA0')
+
+def print_gps_data(line):
+    msg = pynmea2.parse(line)
+    
+    if msg.sentence_type == 'GGA':
+        lat = pynmea2.dm_to_sd(msg.lat)
+        lon = pynmea2.dm_to_sd(msg.lon)
+
+        if msg.lat_dir == 'S':
+            lat = lat * -1
+
+        if msg.lon_dir == 'W':
+            lon = lon * -1
+
+        print(f'{lat},{lon} - from {msg.num_sats} satellites')
+
+while True:
+    line = serial.readline().decode('utf-8')
+
+    while len(line) > 0:
+        print_gps_data(line)
+        line = serial.readline().decode('utf-8')
+
+    time.sleep(1)
